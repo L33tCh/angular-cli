fab7a195fda026d9b0c75192ed2a2918e12818c0
L33tCh <l33tch@gmail.com>
2018-12-14 13:26:41 +0200

    Testing customised schematic. Need to move to a separate schematic module when ready.

--- packages/schematics/angular/component/files/__name@dasherize@if-flat__/__name@dasherize__.component.spec.ts
+++ packages/schematics/angular/component/files/__name@dasherize@if-flat__/__name@dasherize__.component.spec.ts
@@ -1,21 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { <%= classify(name) %>Component } from './<%= dasherize(name) %>.component';
+import { <%= classify(name) %>SharedModule } from './<%= dasherize(name) %>.module';
+// import { of } from 'rxjs';
 
 describe('<%= classify(name) %>Component', () => {
   let component: <%= classify(name) %>Component;
   let fixture: ComponentFixture<<%= classify(name) %>Component>;
+  const oldResetTestingModule = TestBed.resetTestingModule;
+  // let _<%= classify(name) %>Service: <%= classify(name) %>Service;
 
-  beforeEach(async(() => {
+  // const mock<%= classify(name) %>Service = {
+  //   get<%= classify(name) %>s: () => of([]),
+  // };
+
+  beforeAll(done => (async () => {
+    TestBed.resetTestingModule();
     TestBed.configureTestingModule({
-      declarations: [ <%= classify(name) %>Component ]
-    })
-    .compileComponents();
-  }));
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [ <%= classify(name) %>Component ],
+      imports: [<%= classify(name) %>SharedModule],
+      // providers: [
+      //   {
+      //     provide: <%= classify(name) %>Service,
+      //     useValue: mock<%= classify(name) %>Service
+      //   }
+      // ]
+    });
+
+    await TestBed.compileComponents();
+    TestBed.resetTestingModule = () => TestBed;
+  })().then(done).catch(done.fail));
+
+  afterAll(() => {
+    TestBed.resetTestingModule = oldResetTestingModule;
+    TestBed.resetTestingModule();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(<%= classify(name) %>Component);
     component = fixture.componentInstance;
+    // _<%= classify(name) %>Service = fixture.debugElement.injector.get(<%= classify(name) %>Service);
     fixture.detectChanges();
   });
 
--- packages/schematics/angular/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
+++ packages/schematics/angular/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
@@ -4,10 +4,16 @@ import { CommonModule } from '@angular/common';<% } %><% if (routing) { %>
 import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module';<% } %>
 
 @NgModule({
+  exports: []
+})
+export class <%= classify(name) %>SharedModule { }
+
+@NgModule({
   declarations: [],
   imports: [<% if (commonModule) { %>
     CommonModule<%= routing ? ',' : '' %><% } %><% if (routing) { %>
-    <%= classify(name) %>RoutingModule<% } %>
+  <%= classify(name) %>RoutingModule<% } %>,
+    <%= classify(name) %>SharedModule
   ]
 })
 export class <%= classify(name) %>Module { }
